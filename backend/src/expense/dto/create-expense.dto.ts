c50098bd5389ddb87fec6b1bb87bb0aef68af449
nimesh631 <nimeshkayastha067@gmail.com>
2025-09-14 18:22:39 +0545

    feat: add class validator in dto

--- backend/src/expense/dto/create-expense.dto.ts
+++ backend/src/expense/dto/create-expense.dto.ts
@@ -1,13 +1,15 @@
-import { IsDateString, IsNumber, isString, IsString } from "class-validator";
+import { IsDateString, IsNumber, IsString, MinLength } from "class-validator";
 
 export class CreateExpenseDto {
     @IsString()
+        @MinLength(1, { message: 'Title is required' })
     title: string;
 
     @IsNumber()
     amount: number;
 
     @IsString()
+    @MinLength(1, { message: 'Category is required' })
     category: string;
 
     @IsDateString()
